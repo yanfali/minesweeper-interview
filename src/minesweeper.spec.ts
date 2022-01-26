325d7f0ef4607424b818ae7b1d19ea364159d7d4
Yan-Fa Li <yanfali@gmail.com>
2022-01-25 20:53:42 -0800

    Brute force method for ensuring not duplicates
     - store unique tuples in a set as strings then check for existence
     - iterate until you don't get a match
    
    Edge condition: if count > max number of positions, you should probably
    either throw or bail out when you hit the max positions otherwise it
    will loop for 10K iterations.

--- src/minesweeper.spec.ts
+++ src/minesweeper.spec.ts
@@ -1,12 +1,31 @@
-import { initBoard } from "./minesweeper";
+import { initBoard, generatePoints, pointToString } from "./minesweeper";
 describe("test board init", () => {
   it("should initialize the board", () => {
     const board = initBoard();
     expect(board.height).toBe(7);
     expect(board.width).toBe(7);
-    expect(board.board).toHaveLength(7*7);
+    expect(board.board).toHaveLength(7 * 7);
     expect(board.board[0].mine).toBe(false);
     expect(board.board[0].visited).toBe(false);
     expect(board.board[0].score).toBe(0);
+  });
+});
+
+describe("test point generator", () => {
+  it("should only have a single point", () => {
+    const points = generatePoints(7, 7, 1);
+    expect(points).toHaveLength(1);
+  });
+  it("should only have a two points", () => {
+    const points = generatePoints(7, 7, 2);
+    expect(points).toHaveLength(2);
+  });
+  it("should only have a two points and no duplicates", () => {
+    const points = generatePoints(2, 1, 2);
+    expect(points).toHaveLength(2);
+    const a = pointToString(points[0]);
+    const b = pointToString(points[1]);
+    console.log(a,b);
+    expect(a).not.toBe(b);
   });
 });
--- src/minesweeper.ts
+++ src/minesweeper.ts
@@ -28,3 +28,41 @@ export function initBoard(height: number = 7, width: number = 7): Board {
   }
   return board;
 }
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export function pointToString(point: Point): string {
+ return `${point.x},${point.y}`
+}
+
+export function generatePoints(
+  height: number,
+  width: number,
+  count: number
+): Point[] {
+  const pointsMap = new Set<string>();
+  const points: Point[] = [];
+  // things to worry about - repeated points
+  let iterations = 10000;
+  while (points.length < count && iterations > 0) {
+    const x = Math.floor(Math.random() * width);
+    const y = Math.floor(Math.random() * height);
+    const entry = `${x},${y}`;
+    iterations++;
+    if (pointsMap.has(entry)) {
+      continue;
+    }
+    points.push({ x, y });
+    pointsMap.add(entry);
+  }
+
+  if (iterations === 0) {
+    // just for my own interest
+    throw "unable to generate random points in 10K iterations";
+  }
+
+  return points;
+}
